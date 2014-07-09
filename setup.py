a1ee52c485e9f4d6c77a5401a2cf62f675556d86
Matt Wise <matt@nextdoor.com>
2014-07-09 16:58:32 -0700

    Use an https link instead of git+git.

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ setup(
     setup_requires=open('requirements.txt').readlines(),
     install_requires=open('requirements.txt').readlines(),
     dependency_links = [
-      'git+git://github.com/equeny/tornadomail.git@e413b81450bf94b4db3cad1815affb8b2dce6b41#egg=tornadomail-master'
+      'https://github.com/equeny/tornadomail/archive/e413b81450bf94b4db3cad1815affb8b2dce6b41.zip#egg=tornadomail-master',
     ],
     entry_points={
         'console_scripts': [
