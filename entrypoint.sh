b6d2424aa737790982a5702aec61a294921f535a
Mikhail Simin <mikhail@nextdoor.com>
2016-02-16 12:54:08 -0800

    Generalize entrypoint variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,8 @@
 DOCKER_HOST_IP=$(route -n | awk '/UG[ \t]/{print $2}')
 ZOOKEEPER_HOST=${ZOOKEEPER_HOST:-$DOCKER_HOST_IP}
 ZOOKEEPER_PORT=${ZOOKEEPER_PORT:-2181}
-VERBOSE=${VERBOSE:-}
+HTTP_PORT=${HTTP_PORT:-80}
+LOG_LEVEL=${LOG_LEVEL:-info}
+CONFIG_FILE=${CONFIG_FILE:-/sample_config.yaml}
 
-CONFIG_FILE='./sample_config.yaml'
-if [ - ./config.yaml ]; then
-    CONFIG_FILE='./config.yaml'
-fi
-
-zk_monitor -z $ZOOKEEPER_HOST:$ZOOKEEPER_PORT -l INFO -f $CONFIG_FILE
+zk_monitor -z $ZOOKEEPER_HOST:$ZOOKEEPER_PORT -l $LOG_LEVEL -f $CONFIG_FILE -p $HTTP_PORT
